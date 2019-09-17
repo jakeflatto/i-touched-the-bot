2c1fe37bfb053130fc3432db576066bc5ae83f92
Jake Flatto <jakeflatto@gmail.com>
2019-09-16 23:36:24 -0400

    file cleanup

--- commands/iguess.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'iguess',
-	description: `Sends "I guess" image.`,
-	args: false,
-	execute: async (msg, args) => {
-	  	msg.channel.send('https://cdn.discordapp.com/attachments/620157693163864064/620414964561215528/94c.png')
-	}
-}
