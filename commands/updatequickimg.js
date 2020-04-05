4437591cdc4c4b63dedba17a8005346f8bf1ba80
Jake Flatto <jakeflatto@gmail.com>
2020-04-05 19:02:44 -0400

    added update command for quickimages

--- commands/updatequickimg.js
+++ commands/updatequickimg.js
@@ -4,9 +4,10 @@ const quickImages = require('../dynamoquickimages.js')
 const quickMemes = require('../dynamoQuickMemes.js');
 
 module.exports = {
-	name: 'addquickimg',
+	name: 'updatequickimg',
 	args: true,
 	numArgs: 2,
+	guilds: ['jakes-playground-dev', 'jakes-playground'],
 	usage: '[Name] [URL]',
 	example: 'pika https://cdn.discordapp.com/attachments/620157693163864064/620817586233409547/Screen_Shot_2018-10-25_at_11.png',
 	execute: async (msg, args) => {
@@ -14,12 +15,13 @@ module.exports = {
 		url = args[1];
 		let images = await quickImages.getImages();
 		let memes = await quickMemes.getMemes();
-		let quickCommands = images.concat(memes);
-		let quickCommandNames = quickCommands.map(cmd => cmd.name);
-		if (quickCommandNames.includes(name)) {
-			return msg.reply(`Sorry, that name is already taken.\n${prefix}${name} sends: ${quickCommands.filter(cmd => cmd.name == name)[0].url}`);			
+		if (memes.map(cmd => cmd.name).includes(name)) {
+			return msg.reply(`${prefix}${name} is a quickmeme, not a quickimage! It sends: ${memes.filter(cmd => cmd.name == name)[0].url}`);
 		}
-		let add = await quickImages.addImage(name,url);
-			return msg.channel.send(`Quick image added successfully!\n${prefix}${name} now sends ${url}`);
+		if (images.map(cmd => cmd.name).includes(name)) {
+			let update = await quickImages.updateImage(name,url);
+				return msg.channel.send(`Quick image updated successfully!\n${prefix}${name} now sends ${url}`);
+		}
+
 	}
 }
--- dynamoquickimages.js
+++ dynamoquickimages.js
@@ -11,5 +11,22 @@ module.exports = {
 			TableName: "i-touched-the-bot-quick-images"
 		};
 		return dynamo.put(params).promise();
+	},
+	updateImage: (name, url) => {
+		var params = {
+		    TableName: "i-touched-the-bot-quick-images",
+		    Key:{
+		        "name": name
+		    },
+		    ExpressionAttributeValues:{
+		        ":u": url
+		    },
+		    ExpressionAttributeNames: {
+		    	"#u": "url"
+		    },
+		    UpdateExpression: "SET #u = :u",
+		    ReturnValues:"UPDATED_NEW"
+		};
+		return dynamo.update(params).promise().catch((err) => console.log(err));
 	}
 }
