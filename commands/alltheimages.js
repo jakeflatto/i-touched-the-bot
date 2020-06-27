87ddf2c9bcfc6fba143e9d6fe4775e2d739c42c8
Jake Flatto <jakeflatto@gmail.com>
2020-06-27 18:03:49 -0400

    added file types to !alltheimages

--- commands/alltheimages.js
+++ commands/alltheimages.js
@@ -6,7 +6,7 @@ module.exports = {
 	name: 'alltheimages',
 	execute: async (msg, args) => {
 		let images = await quickImages.getImages();
-		images = images.map(img => `${img.name} -- <${img.url}>`).sort();
+		images = images.map(img => `${img.name} (${img.url.includes('youtube') || img.url.includes('tiktok') ? 'video' : img.url.includes('gif') ? 'gif' : 'image'}) -- <${img.url}>`).sort();
 		data = []
 		data.push(`Here is the list of currently recognized quick images:`)
 		data.push(images.join('\n'))
